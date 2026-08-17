b81e275aee91be44b77895e45408daec0ddc75e1
Anshu <anshu@example.com>
2026-08-18 00:26:01 +0530

    Add Google AdSense script

--- src/app/layout.js
+++ src/app/layout.js
@@ -65,6 +65,8 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-V5B5LKW1DN');
           `}
         </Script>
+        {/* Google AdSense */}
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7367387602825591" crossOrigin="anonymous" strategy="afterInteractive" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🇮🇳</text></svg>" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Devanagari:wght@400;600;700&display=swap" rel="stylesheet" />
         <script
